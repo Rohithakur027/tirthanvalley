1ef53763d4659407aca0080ea5a34193a58f593f
Rohit Thakur <rohit.thakur072004@gmail.com>
2026-03-08 19:45:19 +0530

    clarity added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { Navbar } from "@/components/navbar"
 import { Footer } from "@/components/footer"
 import { GoogleAnalytics } from "@next/third-parties/google"
+import Script from "next/script"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -67,6 +68,15 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <GoogleAnalytics gaId="G-HSBP2F8WP9" />
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vsl6iwejl3");
+        `}
+      </Script>
       <body className={cn("min-h-screen font-sans antialiased w-full overflow-x-hidden", fontSans.variable)}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <div className="relative flex min-h-screen flex-col">
